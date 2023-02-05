d49569d41acd3b9d7340a9ac338979992388190f
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 17:42:47 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,17 +1,34 @@
+import logging
+import sys
+import os
+
+log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'geckodriver.log')
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=log_path,
+    filemode='w',
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger('geckodriver')
+
+sys.stderr = sys.stdout = logger
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-import os
-executable_path = GeckoDriverManager().install()
-log_path = "/tmp/geckodriver.log"
 
-service = FirefoxService(executable_path, log_file=log_path)
+executable_path = GeckoDriverManager().install()
+service = FirefoxService(executable_path)
 service.start()
 
 options = webdriver.FirefoxOptions()
-options.add_argument("--headless")
+options.headless = True
 
-browser = webdriver.Firefox(service=service, options=options)
+browser = webdriver.Firefox(options=options, service=service)
+
 
 
 browser.get("https://www.google.com")
