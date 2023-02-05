439bc6991e635fbebd94f8b606713ca38a23e206
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 17:39:41 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,16 +3,16 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 import os
 
-executable_path = GeckoDriverManager().install()
+log_path = "/tmp/geckodriver.log"
 
-log_path = os.path.join(os.getcwd(), "geckodriver.log")
-
-service = FirefoxService(executable_path)
+service = FirefoxService(executable_path, log_file=log_path)
+service.start()
 
 options = webdriver.FirefoxOptions()
-options.add_argument('--headless')
+options.add_argument("--headless")
 
 browser = webdriver.Firefox(service=service, options=options)
+
 
 browser.get("https://www.google.com")
 
