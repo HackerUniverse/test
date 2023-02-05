cb25e6f4f39555e0bdb8b457a976f820dabbcf87
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 20:12:52 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ options.binary_location = '/usr/bin/firefox'
 browser = webdriver.Firefox(options=options)
 
 # navigate to the Google website
-browser.get("https://www.google.com")
+browser.get("https://www.tmforum.org/about-tm-forum/contact-us/")
 
 # get the source code of the page and print it
 source_code = browser.page_source
