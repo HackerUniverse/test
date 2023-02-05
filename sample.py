b691776c0e7055aa1aee272e9e4b12de1824b637
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 17:34:38 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,37 +1,19 @@
-import os
-import sys
-import tempfile
-
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+import os
 
-# Create a temporary directory to store the logs
-log_directory = "/home/ubuntu/"
-
-# Set the log path
-log_path = os.path.join(log_directory, "geckodriver.log")
-
-# Get the path of the geckodriver binary
 executable_path = GeckoDriverManager().install()
 
-# Create the Firefox service
+log_path = os.path.join(os.getcwd(), "geckodriver.log")
+
 service = FirefoxService(executable_path)
 
-# Create the options for the Firefox browser
 options = webdriver.FirefoxOptions()
-options.headless = True
+options.add_argument('--headless')
 
-# Create the Firefox driver
-driver = webdriver.Firefox(service=service, options=options)
+browser = webdriver.Firefox(service=service, options=options)
 
-# Use the driver as needed
-driver.get("https://www.google.com/")
-print(driver.title)
+browser.get("https://www.google.com")
 
-# Close the driver
-driver.quit()
-
-
-
-
+browser.quit()
