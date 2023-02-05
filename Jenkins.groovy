36ec9b8b65c80a725f64320487c20b4f22838118
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 13:31:10 +0530

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -4,10 +4,10 @@ node {
   }
   
   stage('Build Docker image') {
-   sudo sh 'docker build -t selenium-image .'
+   sh 'docker build -t selenium-image .'
   }
   
   stage('Run Selenium script in Docker container') {
-   sudo sh 'docker run -it --rm selenium-image python sample.py'
+   sh 'docker run -it --rm selenium-image python sample.py'
   }
 }
