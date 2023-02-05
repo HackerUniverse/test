93b33f30894d87c5c0698e2367948079ae73f65f
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 17:40:52 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 import os
-
+executable_path = GeckoDriverManager().install()
 log_path = "/tmp/geckodriver.log"
 
 service = FirefoxService(executable_path, log_file=log_path)
