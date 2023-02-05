873118322451fb18c3e396280af15cdd999fbb7c
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 17:17:03 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
 # Use webdriver-manager to manage the geckodriver
-browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+executable_path = GeckoDriverManager().install()
+log_path = '/tmp/geckodriver.log'
+browser = webdriver.Firefox(executable_path=executable_path, service_log_path=log_path)
+
 
 # navigate to the Google website
 browser.get("https://www.google.com")
