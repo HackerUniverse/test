0ccb2777d7a7431ce04db3079a201c72b1008358
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-05 17:47:33 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,36 +1,35 @@
-import logging
-import sys
 import os
-
-log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'geckodriver.log')
-
-logging.basicConfig(
-    level=logging.INFO,
-    filename=log_path,
-    filemode='w',
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
-
-logger = logging.getLogger('geckodriver')
-
-sys.stderr = sys.stdout = logger
+import sys
+import tempfile
 
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
-executable_path = GeckoDriverManager().install()
-service = FirefoxService(executable_path)
-service.start()
+# Create a temporary directory to store the logs
+log_directory = tempfile.mkdtemp()
 
+# Set the log path
+log_path = os.path.join(log_directory, "geckodriver.log")
+
+# Get the path of the geckodriver binary
+executable_path = GeckoDriverManager().install()
+
+# Create the Firefox service
+service = FirefoxService(executable_path)
+
+# Create the options for the Firefox browser
 options = webdriver.FirefoxOptions()
 options.headless = True
 
-browser = webdriver.Firefox(options=options, service=service)
+# Create the Firefox driver
+driver = webdriver.Firefox(service=service, options=options)
+
+# Use the driver as needed
+driver.get("https://www.google.com") 
+# Close the driver
+driver.quit()
 
 
 
-browser.get("https://www.google.com")
 
-browser.quit()
