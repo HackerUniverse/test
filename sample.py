9b24e6447100d435580c606812f93749d8ae42fe
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2023-02-12 20:38:18 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ from seleniumwire import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 # Go to the Google home page
 driver.get('https://example.com')
 
